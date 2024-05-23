4be05676f892226d01f9e5fd477d62ab40d65a63
twkhjl <twkhjl@gmail.com>
2024-05-23 14:14:20 +0800

    修改詳細頁

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,4 +1,65 @@
-<h1>
-    {{ $listing['name'] }}
-</h1>
-<p>{{ $listing['description'] }}</p>
+<x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      title
+    </h2>
+  </x-slot>
+
+
+
+
+  <section class="flex justify-center mt-10">
+
+
+    <div class="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg">
+      <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+          item details
+        </h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">
+          Details and informations
+        </p>
+      </div>
+      <div class="border-t border-gray-200">
+        <dl>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+              title
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {{ $listing['title'] }}
+            </dd>
+          </div>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+              name
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {{ $listing['name'] }}
+            </dd>
+          </div>
+
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+              Email address
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {{ $listing['email'] }}
+            </dd>
+          </div>
+
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+              description
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {{ $listing['description'] }}
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+
+  </section>
+
+</x-app-layout>
