f8cda3bc1401577298dc1e242d4f9cef70138035
twkhjl <twkhjl@gmail.com>
2024-05-24 16:32:09 +0800

    修正詳細頁樣式

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,12 +1,5 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      title
-    </h2>
-  </x-slot>
-
-
-
+  <div class="pt-20"></div>
 
   <section class="flex justify-center mt-10">
 
