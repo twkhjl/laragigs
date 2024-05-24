41a27f51e87779e090a36242c75968052197e0c9
twkhjl <twkhjl@gmail.com>
2024-05-24 23:51:57 +0800

    文字修改

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav class="bg-white border-gray-200 dark:bg-gray-900 fixed w-full z-50">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="{{ asset('images/logo.png') }}" class="h-8" alt="Flowbite Logo" />
+      <img src="{{ asset('images/logo.png') }}" class="h-8" alt="logo" />
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">找工作</span>
     </a>
     @guest
