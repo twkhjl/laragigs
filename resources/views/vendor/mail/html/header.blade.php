04685c1b800f58fc8bff885335f81d86f28b8c95
twkhjl <twkhjl@gmail.com>
2024-05-26 00:10:13 +0800

    修改通知信logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 
-<img src="{{ asset('images/logo.png') }}" class="logo" alt="">
+<img src="{{ asset('images/logo.png') }}" class="logo" style="height:75px;width:75px;" alt="">
 
 </a>
 </td>
