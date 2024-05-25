1f8bd2234a9691f98139e70b5e2d7b715ee85872
twkhjl <twkhjl@gmail.com>
2024-05-26 00:08:38 +0800

    修改通知信樣式

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -77,11 +77,11 @@ class ResetPasswordNotification extends ResetPassword
     protected function buildMailMessage($url)
     {
         return (new MailMessage)
-            ->subject(Lang::get('Reset Password Notification'))
-            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::get('Reset Password!!!'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
-            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
+            ->subject(Lang::get('json.Reset Password Notification'))
+            ->line(Lang::get('json.You are receiving this email because we received a password reset request for your account.'))
+            ->action(Lang::get('json.Reset Password'), $url)
+            ->line(Lang::get('json.This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
+            ->line(Lang::get('json.If you did not request a password reset, no further action is required.'));
     }
 
     /**
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,9 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+
+<img src="{{ asset('images/logo.png') }}" class="logo" alt="">
+
 </a>
 </td>
 </tr>
