09d52cc678cb48ead85da70c5f04685e93a254cb
twkhjl <twkhjl@gmail.com>
2024-05-28 08:46:19 +0800

    修正首頁職缺連結問題

--- resources/views/listings/card-list.blade.php
+++ resources/views/listings/card-list.blade.php
@@ -5,13 +5,13 @@
         $listing->imgUrl = $listing->logo;
     }
 
-    $detailUrl = "/listings/{$listing->id}";
   @endphp
   <div class="m-5">
 
     <div
       class="group mx-2 grid max-w-screen-lg grid-cols-1 space-x-8 overflow-hidden rounded-lg border text-gray-700 shadow transition hover:shadow-lg sm:mx-auto sm:grid-cols-5">
-      <a href="{{ $detailUrl }}" class="col-span-2 text-left text-gray-600 hover:text-gray-700">
+      <a data-modal-target="large-modal{{ $listing->id }}"
+        data-modal-toggle="large-modal{{ $listing->id }}" class="col-span-2 text-left text-gray-600 hover:text-gray-700">
         <div class="group relative h-full w-full overflow-hidden">
           <img src="{{ $listing->imgUrl }}" alt=""
             class="h-full w-full border-none object-cover text-gray-700 transition group-hover:scale-125" />
@@ -22,17 +22,20 @@
         </div>
       </a>
       <div class="col-span-3 flex flex-col space-y-3 pr-8 text-left">
-        <a href="{{ $detailUrl }}" class="mt-3 invisible sm:visible overflow-hidden text-2xl font-semibold">
+        <a href='javascript:void(0)' data-modal-target="large-modal{{ $listing->id }}"
+            data-modal-toggle="large-modal{{ $listing->id }}" class="mt-3 invisible sm:visible overflow-hidden text-2xl font-semibold">
           {{ Str::limit($listing->title, 50, '...') }}
         </a>
-        <a href="{{ $detailUrl }}" class="mt-3 sm:hidden overflow-hidden text-2xl font-semibold">
+        <a href='javascript:void(0)' data-modal-target="large-modal{{ $listing->id }}"
+            data-modal-toggle="large-modal{{ $listing->id }}" class="mt-3 sm:hidden overflow-hidden text-2xl font-semibold">
           {{ Str::limit($listing->title, 15, '...') }}
         </a>
 
         <p class="overflow-hidden text-sm invisible sm:visible">{{ Str::limit($listing->description, 100, '...') }}</p>
         <p class="overflow-hidden text-sm sm:hidden">{{ Str::limit($listing->description, 20, '...') }}</p>
 
-        <a href="{{ $detailUrl }}"
+        <a href='javascript:void(0)' data-modal-target="large-modal{{ $listing->id }}"
+            data-modal-toggle="large-modal{{ $listing->id }}"
           class="text-sm font-semibold text-gray-500 hover:text-gray-700">{{ $listing->company }}</a>
 
         <div class="flex flex-col text-gray-700 sm:flex-row">
